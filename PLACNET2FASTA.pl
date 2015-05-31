5dd9c3babd8bd198a759432e02b7a02f752e6350
tomdeman-bio <tjb.deman@gmail.com>
2015-05-31 15:45:10 -0400

    prints plasmid contigs

--- PLACNET2FASTA.pl
+++ PLACNET2FASTA.pl
@@ -1,53 +1,113 @@
 #!/usr/bin/perl
 
 use strict;
+use Getopt::Long;
 use Array::Utils qw(:all);
 
-my $placnet_csv = shift;
-my $plasmid_fasta = shift;
+my $placnet_csv = "";
+my $plasmid_fasta = "";
+my $original_fasta = "";
+my %placnet_contig_plasmid_link;
+
+GetOptions('csv=s'	=>\$placnet_csv,
+		   'headers=s'	=>\$plasmid_fasta,
+		   'fasta=s'	=>\$original_fasta);
+		   
+&Use unless((-e $placnet_csv) &&(-e $plasmid_fasta) &&(-e $original_fasta));
 
 open CSV, "$placnet_csv" || die "cannot open $placnet_csv for reading";
 open PLA, "$plasmid_fasta" || die "cannot open $plasmid_fasta for reading";
+open FAS, "$original_fasta" || die "cannot open $original_fasta for reading";
 
-#create plasmid header hash
-my %plasmid_dict;
-while (<PLA>) {
-	chomp;
-	my @split_headers = split(/\|/, $_);
-	$split_headers[0] =~ s/^.//;
-	my $accession = "$split_headers[0]"."|"."$split_headers[1]"."|"."$split_headers[2]"."|"."$split_headers[3]"."|";
-	my $organism = "$split_headers[4]";
-	$plasmid_dict{$accession} = $organism;
+#calling the main subroutines and generate main variables
+my ($keys_dict, $plasmids_in_output) = &make_hashs;
+my @contigIDs_of_plasmids = &overlap_detection;
+&create_plasmid_contigs;
+
+sub make_hashs {
+	
+	#create plasmid FASTA header hash
+	my %plasmid_dict;
+	while (<PLA>) {
+		chomp;
+		my @split_headers = split(/\|/, $_);
+		$split_headers[0] =~ s/^.//;
+		my $accession = "$split_headers[0]"."|"."$split_headers[1]"."|"."$split_headers[2]"."|"."$split_headers[3]"."|";
+		my $organism = "$split_headers[4]";
+		$plasmid_dict{$accession} = $organism;
+	}
+	close PLA;
+
+	#store the keys from the plasmid header hash in an array
+	my @keys = keys %plasmid_dict;
+
+	#parse the placnet .csv output file
+	my @plasmids;
+	while (<CSV>) {
+		chomp;
+		my @split_csv = split ("\t", $_);
+		push (@plasmids, $split_csv[1]);
+		$placnet_contig_plasmid_link{$split_csv[1]} = $split_csv[0];
+	}
+	close CSV;
+	
+	return (\@keys, \@plasmids);
 }
 
-#store the keys from the plasmid header hash in an array
-my @keys_dict = keys %plasmid_dict;
+sub overlap_detection {
+	
+	#define overlap between @keys-dict and @plasmids_in_output
+	my @overlap = intersect(@$keys_dict, @$plasmids_in_output);
+	my @plasmid_contigs;
+	foreach my $same (@overlap) {
+		push (@plasmid_contigs, $placnet_contig_plasmid_link{$same});
+	}
 
-#parse the placnet .csv output file
-my @plasmids_in_output;
-my %placnet_contig_plasmid_link;
-while (<CSV>) {
-	chomp;
-	my @split_csv = split ("\t", $_);
-	push (@plasmids_in_output, $split_csv[1]);
-	$placnet_contig_plasmid_link{$split_csv[1]} = $split_csv[0];
+	my @unique_contigs = uniq(@plasmid_contigs);
+	
+	return @unique_contigs;
 }
 
-#define overlap between @keys-dict and @plasmids_in_output
-my @overlap = intersect(@keys_dict, @plasmids_in_output);
-my @plasmid_contigs;
-foreach my $same (@overlap) {
-	push (@plasmid_contigs, $placnet_contig_plasmid_link{$same});
-}
-
-my @unique_contigs = uniq(@plasmid_contigs);
-foreach my $contigs (@unique_contigs) {
-	print $contigs."\n";
+sub create_plasmid_contigs {
+	my $name;
+	my %hash;
+	foreach my $id (@contigIDs_of_plasmids) {
+		 $hash{$id}=1;
+	}
+	
+	while (<FAS>) {
+		chomp;
+   		if (/>(\S+)/) {
+       		$name=$1;
+       		#print $name,"\n";
+       		print $_."\n" if ($hash{$name});
+   		}
+   		else {
+       		print $_."\n" if ($hash{$name});
+   		}
+	}
+	close FAS;
+	
 }
 
 sub uniq {
 	my %hash;
 	return grep { !$hash{$_}++ } @_;
 }
+
+sub Use {
+	die "\n\tUsage::PLACNET2FASTA
 	
+	Written by Tom de Man
 	
+		-csv		The placnet .CSV output file
+	
+		-headers 	A file containing the plasmid FASTA headers, from the plasmid FASTA file used with placnet
+	
+		-fasta		The assembled contig file that served as placnet input
+	\n";
+}
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # PLACNET2FASTA
+
+## This script needs three input files
+
+### 1. The placnet .net.csv file 
+### 2. A FASTA header file containing sequence headers from plasmids only (example header file is included with this script)
+### 3. The assembled contig or scaffold file that was used as input for placnet
+
+### The FASTA header file can be generated using “grep ‘>’ NCBI-plasmid.fasta > header-file.txt”
